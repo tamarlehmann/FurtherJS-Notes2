5a31ce687692b613a1c7e63b71fab4fc2f50f2cb
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-15 12:55:33 +0000

    Finish challenge 6.

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,8 +1,21 @@
 function testNoteControllerCanBeInstantiated() {
   var list = new NoteList();
   var controller = new NoteController(list);
-  assert.isTrue(controller._listView._noteList.length === 1);
+  assert.isTrue(controller instanceof NoteController);
 }
 
 console.log("7. A NoteController can be instantiated.");
 testNoteControllerCanBeInstantiated();
+
+
+function testAppElementContainsNotesAsHTML() {
+  var doubleElement = document.createElement('div');
+  var list = new NoteList();
+  list.addNote("This is a note");
+  var controller = new NoteController(list, doubleElement);
+  controller.htmlInserter();
+  assert.isTrue(controller.element.innerHTML === "<ul><li><div>This is a note</div></li></ul>");
+}
+
+console.log("8. The app element contains notes as HTML string.");
+testAppElementContainsNotesAsHTML();
