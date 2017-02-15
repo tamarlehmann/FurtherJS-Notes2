cef216b6154808b293aa1aed67144734def6d84a
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-15 10:00:47 +0000

    Challenge 2 finished

--- spec/note-list-tests.js
+++ spec/note-list-tests.js
@@ -1,13 +1,16 @@
-// function testListStoresArrayOfNotes() {
-//   var noteList = new NoteList();
-//   assert.isTrue(noteList._list === []);
-// }
-
-// testListStoresArrayOfNotes();
-
 function testDisplayNoteListReturnsListOfNotes() {
   var noteList = new NoteList();
   assert.isTrue(Array.isArray(noteList.displayList()));
 }
 
 testDisplayNoteListReturnsListOfNotes();
+
+function listCanCreateAndStoreNotes() {
+  var noteList = new NoteList();
+  noteList.addNote("This is my note");
+  noteList.addNote("This is my other note");
+
+  assert.isTrue(noteList.displayList().length === 2);
+}
+
+listCanCreateAndStoreNotes();
