3e7a660455d75a526af906b334776be4e1c92020
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-16 12:21:50 +0000

    Finished challenge 9 except for writing the test in note controller to check that text appears in singlenote div

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -11,11 +11,26 @@ testNoteControllerCanBeInstantiated();
 function testAppElementContainsNotesAsHTML() {
   var doubleElement = document.createElement('div');
   var list = new NoteList();
-  list.addNote("This is a note");
+  list.addNote("Hello, I love Javascript!");
   var controller = new NoteController(list, doubleElement);
   controller.htmlInserter();
-  assert.isTrue(controller.element.innerHTML === "<ul><li><div><a href=\"#notes/" + 0 + "\">" + "This is a note</a></div></li></ul>");
+  assert.isTrue(controller.element.innerHTML === "<ul><li><div><a href=\"#notes/" + 0 + "\">" + "Hello, I love Javasc</a></div></li></ul>");
 }
 
 console.log("9. The app element contains notes as HTML string.");
 testAppElementContainsNotesAsHTML();
+
+// function testClickedNoteDisplaysSingleNote(){
+//   var list = new NoteList();
+//   list.addNote('Hello, I love Javascript!');
+//   var linkDouble = document.createElement('app');
+//   var noteDouble = document.createElement('singlenote');
+//   var controller = new NoteController(list, linkDouble);
+//   controller.htmlInserter();
+//   controller.urlChangeShowsNote();
+//   linkDouble.querySelector('a').click();
+//   assert.isTrue(noteDouble.innerHTML === "Hello, I love Javascript!");
+// };
+//
+// console.log("11. The singlenote element contains the full text of the note.");
+// testClickedNoteDisplaysSingleNote()
